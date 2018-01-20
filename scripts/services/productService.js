49c5c835f0f436f6a1d78693463b4bc85ddf1718
liuruigang <275618939@qq.com>
2018-01-20 17:14:16 +0800

    增加服务管理

--- scripts/services/productService.js
+++ scripts/services/productService.js
@@ -3,6 +3,62 @@ define(['services/services', 'services/commonService'],
         services.factory('ProductService', ['$http', '$q', 'CommonService',
             function ($http, $q, commonService) {
                 return {
+                    /*添加产品类别*/
+                    classfyCreate: function (data) {
+                        var deferred = $q.defer();
+                        $http({
+                            url: "http://" + commonService.getServerUrl() + "/app/classify",
+                            method: "put",
+                            data: "name=" + data.name
+                        }).success(function (data, status, headers, config) {
+                            deferred.resolve(data);
+                        }).error(function (data, status, headers, config) {
+                            deferred.reject(data);
+                        });
+                        return deferred.promise;
+                    },
+                    /*添加产品信息*/
+                    productCreate: function (data) {
+                        var deferred = $q.defer();
+                        $http({
+                            url: "http://" + commonService.getServerUrl() + "/app/product",
+                            method: "put",
+                            data: "classifyId=" + data.classifyId + "&name=" + data.name + "&price=" + data.price + "&retain=" + data.retain
+                        }).success(function (data, status, headers, config) {
+                            deferred.resolve(data);
+                        }).error(function (data, status, headers, config) {
+                            deferred.reject(data);
+                        });
+                        return deferred.promise;
+                    },
+                    /*更新产品信息*/
+                    productUpdate: function (data) {
+                        var deferred = $q.defer();
+                        $http({
+                            url: "http://" + commonService.getServerUrl() + "/app/product",
+                            method: "post",
+                            data: "id=" + data.id + "&classifyId=" + data.classifyId + "&name=" + data.name + "&price=" + data.price + "&retain=" + data.retain
+                        }).success(function (data, status, headers, config) {
+                            deferred.resolve(data);
+                        }).error(function (data, status, headers, config) {
+                            deferred.reject(data);
+                        });
+                        return deferred.promise;
+                    },
+                    /*删除产品信息*/
+                    productDelete: function (data) {
+                        var deferred = $q.defer();
+                        $http({
+                            url: "http://" + commonService.getServerUrl() + "/app/product/" + data.classifyId + "/" + data.id,
+                            method: "delete",
+                            data: {}
+                        }).success(function (data, status, headers, config) {
+                            deferred.resolve(data);
+                        }).error(function (data, status, headers, config) {
+                            deferred.reject(data);
+                        });
+                        return deferred.promise;
+                    },
                     /*查询产品类别信息*/
                     queryClassfyInfo: function () {
                         var deferred = $q.defer();
@@ -21,7 +77,7 @@ define(['services/services', 'services/commonService'],
                     queryProductInfo: function (classId) {
                         var deferred = $q.defer();
                         $http({
-                            url: "http://" + commonService.getAppServerUrl() + "/app/product/"+classId,
+                            url: "http://" + commonService.getAppServerUrl() + "/app/product/" + classId,
                             method: "get",
                             data: {}
                         }).success(function (data, status, headers, config) {
