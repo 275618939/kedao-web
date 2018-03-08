33ba7ef27b19ee61c29288be0d6de4d6aba9ed7a
liuruigang <275618939@qq.com>
2018-03-08 22:18:24 +0800

    增加qrcode

--- scripts/register.js
+++ scripts/register.js
@@ -1,15 +1,19 @@
-
 require.config({
     paths: {
         angular: 'vendor/angular',
         angularRoute: 'vendor/angular-route',
         jquery: 'vendor/jquery',
+        qrcode: 'vendor/qrcode.min',
         jqueryMd5: 'vendor/jQuery.md5',
         domReady: 'vendor/domReady'
     },
     shim: {
         jquery: {
             exports: 'jquery'
+        },
+        qrcode: {
+            deps: ['jquery'],
+            exports: 'qrcode'
         },
         jqueryMd5: {
             deps: ['jquery'],
@@ -30,7 +34,7 @@ require.config({
 //
 require([
         'angular', 'angularRoute',
-        'app', 'domReady', 'jquery', 'jqueryMd5',
+        'app', 'domReady', 'jquery', 'jqueryMd5', 'qrcode',
         'filters/intervalFilters',
         'controllers/userController',
         'controllers/commonController',
